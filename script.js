ba6b0062d707c547644ee6132a3ca48cca0f8533
nathalierothe <nathalierothexxx@gmail.com>
2024-12-25 04:32:28 -0600

    made new codes with new text color

---
+++ script.js
@@ -0,0 +1,4 @@
+// Script to show the pop-up reminder after a delay
+setTimeout(function() {
+    alert("🎉 Nathalie's Birthday Stream is coming up on January 6th, 10 AM CST! 🎂");
+}, 5000); // Pop-up after 5 seconds
